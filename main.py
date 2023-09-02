571393ca601196928a95a3dd57af9111ff28df90
the drunkingmaster <libraryusertrackingsystem@gmail.com>
2023-09-02 19:01:05 +0530

    update commit

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from random import randint
 
 print("hey guys")
 
-for i in range(1,5):
+for i in range(1,365):
     for j in range(1,randint(1,3)):
         d = str(i) + ' days ago'
         with open('file.txt','a') as f:
             f.write(d)
         os.system('git add .')
         os.system('git commit --date="'+d+'" -m "update commit"')
-
+os.system('git push origin main')
 
